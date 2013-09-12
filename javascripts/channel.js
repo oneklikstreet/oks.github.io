2b34620dd73d94be83db4d9a9d8ac0aed0c61f6c
oneklikstreet <rishikulkarni@hrishikeshs-MacBook-Pro.local>
2013-09-12 14:06:17 +0530

    update git

--- javascripts/channel.js
+++ javascripts/channel.js
@@ -4,29 +4,29 @@ define ( [],  function () {
     var processMessage;
     var socketOpen = function() {
             socket.active = 1;
-            console.log('web socket connection is successful\n');
+            console.log('socketOpen ' + 'web socket connection is successful\n');
             //video_call();
         }
 
     var socketError = function(err) {
             socket.active = 0;
-            console.log('web socket connection error'+err);
+            console.log('socketError '+ 'web socket connection error'+err);
         }
 
     var socketRecv = function(msg) {
-            console.log('Server:'+msg.data);
+            console.log('socketRecv Server: ' + msg.data);
             //processSignalingMessage(msg.data);
             processMessage(msg.data);
         }
 
     var socketClose = function(msg) {
             socket.active = 0;
-            console.log('web socket is disconnected ' + msg);
+            console.log('socketClose ' + 'web socket is disconnected ' + msg);
         }
 
     return {
         openChannel: function (processMsg) {
-            console.log("Opening channel.");
+            console.log('openChannel ' + 'Opening channel.');
             socket = new WebSocket('ws://119.81.19.90:8000/join_conf/', ['soap', 'xmpp', 'sip']);
             socket.binaryType = 'arraybuffer';
             processMessage = processMsg;
@@ -37,20 +37,20 @@ define ( [],  function () {
         },
         sendMessage : function (msg) {
             if(socket.active){
-                console.log('Browser -> Server' + msg);
+                console.log('sendMessage ' + 'Browser -> Server: ' + msg);
                 socket.send(msg);
             } else {
                 if(socket_connection_wait){
                     socket_connection_wait -= 1;
                     //waiting for connection to get ready
                     setTimeout( function() { 
-                                    console.log("waiting for connection\n"); 
-                                    sendMessage(msg);
-                                    }, 
+                                console.log('sendMessage ' + "waiting for connection\n"); 
+                                sendMessage(msg);
+                            }, 
                                 1000);
                     return;
                 }
-                console.log(' Error: Lost connection with the service. Please check your network setttings.');
+                console.log('sendMessage ' + ' Error: Lost connection with the service. Please check your network setttings.');
                 alert(' Error: Lost connection with the service. Please check your network setttings.');
             }
         }
